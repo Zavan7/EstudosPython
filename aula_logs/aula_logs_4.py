835e81c672c4c85bba49e26c72eab2d7a506fc27
Zavan7 <vitorzavanj@gmail.com>
2025-02-15 00:19:36 -0300

    add log filtering to exclude sensitive information and update log format

--- aula_logs/aula_logs_4.py
+++ aula_logs/aula_logs_4.py
@@ -1,12 +1,24 @@
 from loguru import logger
 from pathlib import Path
+import sys
 
 FILE_LOG = Path(__file__).parent / '.log'
 
-logger.add(FILE_LOG)
+logger.remove()
+
+def filtro_senha(record):
+    if 'senha' in record['message']:
+        return False
+    return True
+
+logger.add(
+    sys.stderr,
+    format='{time} - <r>{level}</r> - <g>{message}</g> - {file} - {line}', 
+    filter= filtro_senha
+)
 
 logger.debug('DEBUG')
 logger.info('INFO')
 logger.warning ('WARNING')
 logger.error('ERROR')
-logger.critical ('CRITICAL')
+logger.critical('senha')
