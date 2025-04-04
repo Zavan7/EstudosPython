b51dec60dc66820699e97738a113fd06cb5709ab
Zavan7 <vitorzavanj@gmail.com>
2025-04-04 02:00:20 -0300

    replace aula_mysql_01.py with multiple SQLite management scripts for improved functionality and data handling

---
+++ aula_sql/aula_sqlite_02.py
@@ -0,0 +1,47 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'clientes'
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+# CUIDADO: fazendo delete sem where
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+
+connection.commit()
+
+cursor.execute(
+     f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+ )
+connection.commit()
+
+# Cria tabelas
+cursor.execute(
+    F'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'peso REAL'
+    ')'
+)
+connection.commit()
+
+# Registra valores nas colunas
+# CUIDADO: sql injection
+sql =(
+    f'INSERT INTO {TABLE_NAME} '
+    '(name, peso) '
+    'VALUES '
+    '(?, ?) '
+)
+cursor.executemany(sql, [['Isabelle', 45],['Vitor', 80]])
+connection.commit()
+
+cursor.close()
+connection.close()
